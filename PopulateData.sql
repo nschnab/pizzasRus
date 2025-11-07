b8642a3abd20c6fce60b71203265bb093329814a
Nolen Schnabel <nschnab@clemson.edu>
2025-11-07 10:41:12 -0500

    populate data

---
+++ PopulateData.sql
@@ -0,0 +1,15 @@
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (1, 'Pepperoni', 1.25, 0.2, 100, 50, 2, 2.75, 3.5, 4.5);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (2, 'Sausage', 1.25, 0.15, 100, 50, 2.5, 3, 3.5, 4.25);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (3, 'Ham', 1.5, 0.15, 78, 25, 2, 2.5, 3.25, 4);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (4, 'Chicken', 1.75, 0.25, 56, 25, 1.5, 2, 2.25, 3);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (5, 'Green Pepper', 0.5, 0.02, 79, 25, 1, 1.5, 2, 2.5);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (6, 'Onions', 0.5, 0.02, 85, 25, 1, 1.5, 2, 2.75);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (7, 'Roma Tomato', 0.75, 0.03, 86, 10, 2, 3, 3.5, 4.5);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (8, 'Mushrooms', 0.75, 0.1, 52, 50, 1.5, 2, 2.5, 3);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (9, 'Black Olives', 0.6, 0.1, 39, 25, 0.75, 1, 1.5, 2);
+insert into topping (topping_TopID, topping_TopName, topping_CustPrice, topping_BusPrice, topping_CutINVT, topping_MinINVT, topping_SmallAMT, topping_MedAMT, topping_LgAMT, topping_XLAMT) values (10, 'Pineapple', 1, 0.25, 15, 0, 1, 1.25, 1.75, 2);
+
+
+
+
+
