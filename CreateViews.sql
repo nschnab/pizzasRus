83c3d5409bb7590bcd4fc48a68fd88578b367ed9
Nolen Schnabel <nschnab@clemson.edu>
2025-11-07 08:12:29 -0500

    create view

--- CreateViews.sql
+++ CreateViews.sql
@@ -5,7 +5,10 @@ from topping
 order by `ToppingCount` desc, topping_TopName;
 
 drop view if exists ProfitByPizza;
-
-
+create view ProfitByPizza as
+select p.pizza_Size as Size, p.pizza_CrustType as Crust, (p.pizza_CustPrice - p.pizza_BusPrice) * d.discount_Amount as Profit, month(p.pizza_PizzaDate) as OrderMonth
+from pizza p
+join pizza_discount pd on pd.pizza_PizzaID = p.pizza_PizzaID
+join discount d on d.discount_DiscountID = pd.discount_DiscountID;
 
 drop view if exists ProfitByOrderType;
