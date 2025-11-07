a414a3304ebba381b6010a3e0987c0e5bab74b91
emmaabraham04 <156918206+emmaabraham04@users.noreply.github.com>
2025-11-07 10:59:23 -0500

    Update DropTables.sql
    adding the drop table and views

---
+++ DropTables.sql
@@ -0,0 +1,18 @@
+use PizzaDB;
+
+drop table customer;
+drop table ordertable;
+drop table pickup;
+drop table delivery;
+drop table dinein;
+drop table discount;
+drop table order_discount;
+drop table pizza;
+drop table pizza_discount;
+drop table baseprice;
+drop table topping;
+drop table pizza_topping;
+
+drop view ToppingPopularity;
+drop view ProfitByPizza;
+drop view ProfitByOrderType;
