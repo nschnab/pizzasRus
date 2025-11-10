9b7dd9d454750ba9324ed37c7f2b80fd93b86269
Nolen Schnabel <nschnab@clemson.edu>
2025-11-10 07:54:38 -0500

    fix for autograder

--- CreateTables.sql
+++ CreateTables.sql
@@ -12,62 +12,63 @@ GRANT ALL ON PizzaDB.* TO 'dbtester';
 
 -- Create Tables
 create table customer (
-	customer_CustID int primary key not null,
+	customer_CustID int primary key not null auto_increment,
     customer_FName varchar(30) not null,
     customer_LName varchar(30) not null,
-    customer_PhoneNum varchar(30)
+    customer_PhoneNum varchar(30) not null
 );
 
 create table ordertable (
-	ordertable_OrderID int primary key not null,
-    customer_CustID int not null,
-    ordertable_OrderType varchar(30),
-    ordertable_OrderDateTime datetime,
-    ordertable_CustPrice decimal (5,2),
-    ordertable_BusPrice decimal (5,2),
-    ordertable_isComplete boolean,
+	ordertable_OrderID int primary key not null auto_increment,
+    customer_CustID int,
+    ordertable_OrderType varchar(30) not null,
+    ordertable_OrderDateTime datetime not null,
+    ordertable_CustPrice decimal (5,2) not null,
+    ordertable_BusPrice decimal (5,2) not null,
+    ordertable_isComplete boolean default 0,
     constraint foreign key (customer_CustID) references customer(customer_CustID)
 );
 
 create table pickup (
 	ordertable_OrderID int primary key not null,
-    pickup_IsPickedUp boolean,
+    pickup_IsPickedUp boolean not null default 0,
     constraint foreign key (ordertable_OrderID) references ordertable(ordertable_OrderID)
 );
 
 create table delivery (
 	ordertable_OrderID int primary key not null,
-	delivery_HouseNum int,
-    delivery_Street varchar(30),
-    delivery_City varchar(30),
-    delivery_State varchar(30),
-    delivery_Zip int,
-    delivery_IsDelivered boolean,
+	delivery_HouseNum int not null,
+    delivery_Street varchar(30) not null,
+    delivery_City varchar(30) not null,
+    delivery_State varchar(30) not null,
+    delivery_Zip int not null,
+    delivery_IsDelivered boolean not null default 0,
     constraint foreign key (ordertable_OrderID) references ordertable(ordertable_OrderID)
 );
 
 create table dinein (
 	ordertable_OrderID int primary key not null,
-    dinein_TableNum int,
+    dinein_TableNum int not null,
     constraint foreign key (ordertable_OrderID) references ordertable(ordertable_OrderID)
 );
 
 create table discount (
-	discount_DiscountID int primary key not null,
-    discount_DiscountName varchar(30),
-    discount_Amount decimal (5,2),
-    discount_IsPercent boolean
+	discount_DiscountID int primary key not null auto_increment,
+    discount_DiscountName varchar(30) not null,
+    discount_Amount decimal (5,2) not null,
+    discount_IsPercent boolean not null
 );
 
 create table order_discount (
 	ordertable_OrderID int not null,
     discount_DiscountID int not null,
+    primary key (ordertable_OrderID, discount_DiscountID),
     constraint foreign key (ordertable_OrderID) references ordertable(ordertable_OrderID),
     constraint foreign key (discount_DiscountID) references discount(discount_DiscountID)
 );
 
 create table pizza (
-	pizza_PizzaID int primary key not null,
+	pizza_PizzaID int primary key not null auto_increment,
     pizza_Size varchar(30),
     pizza_CrustType varchar(30),
     pizza_PizzaState varchar(30),
@@ -75,12 +76,15 @@ create table pizza (
     pizza_CustPrice decimal (5,2),
     pizza_BusPrice decimal (5,2),
     ordertable_orderID int not null,
-    constraint foreign key (ordertable_orderID) references ordertable(ordertable_orderID)
+    constraint foreign key (ordertable_orderID) references ordertable(ordertable_orderID),
+    constraint foreign key (pizza_CrustType) references baseprice(baseprice_CrustType),
+    constraint foreign key (pizza_Size) references baseprice(baseprice_Size)
 );
 
 create table pizza_discount (
 	pizza_PizzaID int not null,
     discount_DiscountID int not null,
+    primary key (pizza_PizzaID, discount_DiscountID),
     constraint foreign key (pizza_PizzaID) references pizza(pizza_PizzaID),
     constraint foreign key (discount_DiscountID) references discount(discount_DiscountID)
 );
@@ -88,28 +92,29 @@ create table pizza_discount (
 create table baseprice (
 	baseprice_Size varchar(30) not null,
     baseprice_CrustType varchar(30) not null,
-    baseprice_CustPrice decimal (5,2),
-    baseprice_BusPrice decimal(5,2),
+    baseprice_CustPrice decimal (5,2) not null,
+    baseprice_BusPrice decimal(5,2) not null,
     primary key (baseprice_Size, baseprice_CrustType)
 );
 
 create table topping (
-	topping_TopID int primary key not null,
-	topping_TopName varchar(30),
-    topping_SmallAMT decimal(5,2),
-    topping_MedAMT decimal(5,2),
-    topping_LgAMT decimal(5,2),
-    topping_XLAMT decimal(5,2),
-    topping_CustPrice decimal(5,2),
-    topping_BusPrice decimal(5,2),
-    topping_MinINVT int,
-    topping_CutINVT int
+	topping_TopID int primary key not null auto_increment,
+	topping_TopName varchar(30) not null,
+    topping_SmallAMT decimal(5,2) not null,
+    topping_MedAMT decimal(5,2) not null,
+    topping_LgAMT decimal(5,2) not null,
+    topping_XLAMT decimal(5,2) not null,
+    topping_CustPrice decimal(5,2) not null,
+    topping_BusPrice decimal(5,2) not null,
+    topping_MinINVT int not null,
+    topping_CurINVT int not null
 );
 
 create table pizza_topping (
 	pizza_PizzaID int not null,
     topping_TopID int not null,
-    pizza_topping_IsDouble int,
+    pizza_topping_IsDouble int not null,
+    primary key (pizza_PizzaID, topping_TopID),
     constraint foreign key (pizza_PizzaID) references pizza(pizza_PizzaID),
     constraint foreign key (topping_TopID) references topping(topping_TopID)
 );
--- CreateViews.sql
+++ CreateViews.sql
@@ -1,17 +1,17 @@
-drop view if exists ToppingPopularity;
+-- drop view if exists ToppingPopularity;
 create view ToppingPopularity as
 select topping_TopName as "Topping", (topping_SmallAMT + topping_MedAMT + topping_LgAMT + topping_XLAMT) as `ToppingCount`
 from topping
 order by `ToppingCount` desc, topping_TopName;
 
-drop view if exists ProfitByPizza;
+-- drop view if exists ProfitByPizza;
 create view ProfitByPizza as
 select p.pizza_Size as Size, p.pizza_CrustType as Crust, (p.pizza_CustPrice - p.pizza_BusPrice) * d.discount_Amount as Profit, month(p.pizza_PizzaDate) as OrderMonth
 from pizza p
 join pizza_discount pd on pd.pizza_PizzaID = p.pizza_PizzaID
 join discount d on d.discount_DiscountID = pd.discount_DiscountID;
 
-drop view if exists ProfitByOrderType;
+-- drop view if exists ProfitByOrderType;
 create view ProfitByOrderType as
-select o.ordertable_OrderType as CustomerType, o.ordertable_OrderDateTime as OrderMonth, o.ordertable_CustPrice as TotalOrderPrice, o.ordertable_BusPrice as OrderTotalCost, o.ordertable_CustPrice - o.ordertable_BusPrice as Profit
+select o.ordertable_OrderType as CustomerType, string(o.ordertable_OrderDateTime) as OrderMonth, o.ordertable_CustPrice as TotalOrderPrice, o.ordertable_BusPrice as OrderTotalCost, o.ordertable_CustPrice - o.ordertable_BusPrice as Profit
 from ordertable o;
