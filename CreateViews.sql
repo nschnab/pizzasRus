d997cfb5292e5515408e1070a3f7d4b06350470a
Nolen Schnabel <nschnab@clemson.edu>
2025-11-15 13:28:31 -0500

    update views

--- CreateViews.sql
+++ CreateViews.sql
@@ -1,28 +1,48 @@
--- drop view if exists ToppingPopularity;
+drop view if exists ToppingPopularity;
 create view ToppingPopularity as
 select 
 	topping_TopName as "Topping", 
-    (topping_SmallAMT + topping_MedAMT + topping_LgAMT + topping_XLAMT) as `ToppingCount`
-from topping
+	count(*) as `ToppingCount`
+from topping t
+join pizza_topping pt on pt.topping_TopID = t.topping_TopID
+join pizza p on p.pizza_pizzaID = tp.pizza_pizzaID
+group by t.topping_TopName
 order by `ToppingCount` desc, topping_TopName;
 
--- drop view if exists ProfitByPizza;
+drop view if exists ProfitByPizza;
 CREATE VIEW ProfitByPizza AS
 SELECT 
     p.pizza_Size AS Size,
     p.pizza_CrustType AS Crust,
     (p.pizza_CustPrice - p.pizza_BusPrice) * d.discount_Amount AS Profit,
-    DATE_FORMAT(p.pizza_PizzaDate, '%m') AS OrderMonth
+    DATE_FORMAT(p.pizza_PizzaDate, '%m/%Y') AS `OrderMonth`
 FROM pizza p
 JOIN pizza_discount pd ON pd.pizza_PizzaID = p.pizza_PizzaID
-JOIN discount d ON d.discount_DiscountID = pd.discount_DiscountID;
+JOIN discount d ON d.discount_DiscountID = pd.discount_DiscountID
+group by `OrderMonth`;
 
--- drop view if exists ProfitByOrderType;
+
+DROP VIEW IF EXISTS ProfitByOrderType;
 CREATE VIEW ProfitByOrderType AS
-SELECT 
-    o.ordertable_OrderType AS CustomerType,
-    DATE_FORMAT(o.ordertable_OrderDateTime, '%m-%d') AS OrderMonth,
-    o.ordertable_CustPrice - o.ordertable_BusPrice AS Profit,
-    o.ordertable_BusPrice AS TotalOrderCost,
-    o.ordertable_CustPrice AS TotalOrderPrice
-FROM ordertable o;
+SELECT * FROM (
+    SELECT 
+        o.ordertable_OrderType AS CustomerType,
+        DATE_FORMAT(o.ordertable_OrderDateTime, '%m/%Y') AS OrderMonth,
+        SUM(o.ordertable_CustPrice) AS TotalOrderPrice,
+        SUM(o.ordertable_BusPrice) AS TotalOrderCost,
+        SUM(o.ordertable_CustPrice - o.ordertable_BusPrice) AS Profit
+    FROM ordertable o
+    GROUP BY o.ordertable_OrderType, OrderMonth
+
+    UNION ALL
+
+    SELECT 
+        '' AS CustomerType,
+        'Grand Total' AS OrderMonth,
+        SUM(o.ordertable_CustPrice) AS TotalOrderPrice,
+        SUM(o.ordertable_BusPrice) AS TotalOrderCost,
+        SUM(o.ordertable_CustPrice - o.ordertable_BusPrice) AS Profit
+    FROM ordertable o
+) AS combined
+ORDER BY CustomerType;
+
